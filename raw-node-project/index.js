261f9b288c8f07b1c312dd476a01283aea996dc0
Abdur Razzak <67901115+devrazzak@users.noreply.github.com>
2026-04-13 16:14:00 +0600

    file add, edit, delete

--- raw-node-project/index.js
+++ raw-node-project/index.js
@@ -1,20 +1,23 @@
 // dependencies
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const env = require("./helpers/environment");
+const data = require("./lib/data");
 
 // app object - module scaffolding
 const app = {};
 
-// configuration
-app.config = {
-  port: 3000,
-};
+// testing file system
+// TODO: Remove later
+data.delete("test", "newFile", (err) => {
+  console.log(err);
+});
 
 // create server
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
-  server.listen(app.config.port, () => {
-    console.log(`listening to port ${app.config.port}`);
+  server.listen(env.port, () => {
+    console.log(`listening to port ${env.port}`);
   });
 };
 
---
+++ raw-node-project/lib/data.js
@@ -0,0 +1,95 @@
+// Dependencies
+const fs = require("fs");
+const path = require("path");
+const { callbackify } = require("util");
+
+// module scaffolding
+const lib = {};
+
+// base directory of the data folder
+lib.baseDir = path.join(__dirname, "/../.data/");
+
+// write data to file
+lib.create = (dir, file, data, callback) => {
+  // open file for writing
+  fs.open(`${lib.baseDir + dir}/${file}.json`, "wx", (err, fileDescriptor) => {
+    if (!err && fileDescriptor) {
+      // convert data to string
+      const stringData = JSON.stringify(data);
+
+      // write data to file and then close it
+      fs.writeFile(fileDescriptor, stringData, (err2) => {
+        if (!err2) {
+          fs.close(fileDescriptor, (err3) => {
+            if (!err3) {
+              callback(false);
+            } else {
+              callback("Error closing the new file!");
+            }
+          });
+        } else {
+          callback("Error writing to new file");
+        }
+      });
+    } else {
+      callback("could not create new file, it may already exists!");
+    }
+  });
+};
+
+// Read data from file
+lib.read = (dir, file, callback) => {
+  fs.readFile(`${lib.baseDir + dir}/${file}.json`, "utf8", (err, data) => {
+    callback(err, data);
+  });
+};
+
+// Update existing file
+lib.update = (dir, file, data, callback) => {
+  // file open for writing
+  fs.open(`${lib.baseDir + dir}/${file}.json`, "r+", (err, fileDescriptor) => {
+    if (!err && fileDescriptor) {
+      // convert the data to string
+      const stringData = JSON.stringify(data);
+
+      // truncate the file
+      fs.ftruncate(fileDescriptor, (err2) => {
+        if (!err2) {
+          // write to the file and close it
+          fs.writeFile(fileDescriptor, stringData, (err3) => {
+            if (!err3) {
+              // close the file
+              fs.close(fileDescriptor, (err4) => {
+                if (!err4) {
+                  callback(false);
+                } else {
+                  callback("Error closing file");
+                }
+              });
+            } else {
+              callback("Error writing file!");
+            }
+          });
+        } else {
+          callback("Error truncating file!");
+        }
+      });
+    } else {
+      callback("Error updating. FIle may not exist");
+    }
+  });
+};
+
+// delete existing file
+lib.delete = (dir, file, callback) => {
+  // unlink file
+  fs.unlink(`${lib.baseDir + dir}/${file}.json`, (err) => {
+    if (!err) {
+      callback(false);
+    } else {
+      callback("Error deleting file!");
+    }
+  });
+};
+
+module.exports = lib;
