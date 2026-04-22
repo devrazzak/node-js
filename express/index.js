a9e25e2b77a2aed0b894ae143c103c63db6f426c
Abdur Razzak <67901115+devrazzak@users.noreply.github.com>
2026-04-22 23:36:26 +0600

    middleware

--- express/index.js
+++ express/index.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.use(express.json());
 
+const myMiddleware1 = (req, res, next) => {
+  console.log("This is middleware 1");
+  next();
+};
+
+const myMiddleware2 = (req, res, next) => {
+  console.log("This is middleware 2");
+  next();
+};
+
+app.use(myMiddleware1);
+app.use(myMiddleware2);
+
 app.get("/", (req, res) => {
   res.send("This is home page!");
 });
